51bf168ddd5ad6a236ec6a552381891f91f672ec
Robert Bullfrog <williampikus@gmail.com>
2022-04-25 18:57:14 -0700

    Test

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -35,10 +35,10 @@ router.get('/post/:id', async (req, res) => {
           model: User,
           attributes: ['name'],
         },
-        {
-          model: Comment,
-          attributes: ['id', 'content', 'post_id', 'user_id'],
-        },
+        // {
+        //   model: Comment,
+        //   attributes: ['id', 'content', 'post_id', 'user_id'],
+        // },
         // {
         //   model: Comment,
         //   attributes: ['id', 'content', 'post_id', 'user_id'],
